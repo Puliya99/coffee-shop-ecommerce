6b9a0108210dbac52cdf6e1fd9e0acd31538ee10
Pulinda Mathagadeera <122967894+Puliya99@users.noreply.github.com>
2025-03-19 18:18:14 +0530

    Refactor App component to implement routing with protected and admin routes for enhanced navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import { CartProvider } from './context/CartContext'
+import PrivateRoute from './components/routes/PrivateRoute'
+import AdminRoute from './components/routes/AdminRoute'
+
+// Pages
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
+import CartPage from './pages/CartPage'
+import CheckoutPage from './pages/CheckoutPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import OrderSuccessPage from './pages/OrderSuccessPage'
+import UserDashboardPage from './pages/UserDashboardPage'
+import AdminDashboardPage from './pages/AdminDashboardPage'
+import ProductFormPage from './pages/ProductFormPage'
+import OrdersManagementPage from './pages/OrdersManagementPage'
+
+// Components
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <AuthProvider>
+        <CartProvider>
+          <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <main className="container mx-auto flex-grow px-4 py-8">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/product/:id" element={<ProductPage />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+
+                {/* Protected Routes */}
+                <Route
+                  path="/checkout"
+                  element={
+                    <PrivateRoute>
+                      <CheckoutPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/order-success"
+                  element={
+                    <PrivateRoute>
+                      <OrderSuccessPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/my-account"
+                  element={
+                    <PrivateRoute>
+                      <UserDashboardPage />
+                    </PrivateRoute>
+                  }
+                />
+
+                {/* Admin Routes */}
+                <Route
+                  path="/admin"
+                  element={
+                    <AdminRoute>
+                      <AdminDashboardPage />
+                    </AdminRoute>
+                  }
+                />
+                <Route
+                  path="/admin/product/:id?"
+                  element={
+                    <AdminRoute>
+                      <ProductFormPage />
+                    </AdminRoute>
+                  }
+                />
+                <Route
+                  path="/admin/orders"
+                  element={
+                    <AdminRoute>
+                      <OrdersManagementPage />
+                    </AdminRoute>
+                  }
+                />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </CartProvider>
+      </AuthProvider>
+    </Router>
+  )
 }
 
-export default App;
+export default App
